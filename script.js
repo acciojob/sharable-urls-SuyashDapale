92f6efc86063ec7957997426fdacfa53ebccfdb5
Suyash Dapale <77561910+SuyashDapale@users.noreply.github.com>
2026-05-25 18:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+const button = document.getElementById("button");
+
+        button.addEventListener("click", function () {
+
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+
+            let url = "https://localhost:8080/";
+
+            let params = [];
+
+            if (name) {
+                params.push(`name=${name}`);
+            }
+
+            if (year) {
+                params.push(`year=${year}`);
+            }
+
+            if (params.length > 0) {
+                url += "?" + params.join("&");
+            }
+
+            document.getElementById("url").innerText = url;
+        });
